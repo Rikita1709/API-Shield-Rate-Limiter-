843ac5c9cec08de39aece91f8bf60da8f2d44ee0
Rikita1709 <rikitamaiti@gmail.com>
2026-06-05 22:50:47 +0530

    Update RateLimiterService.java

--- api-shield-backend/api-shield-backend/src/main/java/com/apishield/ratelimiter/RateLimiterService.java
+++ api-shield-backend/api-shield-backend/src/main/java/com/apishield/ratelimiter/RateLimiterService.java
@@ -1,34 +1,52 @@
 package com.apishield.ratelimiter;
 
-import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
+import com.apishield.service.StatisticsService;
 
 @Service
 public class RateLimiterService {
 
-private final Map<String, Integer> requestCounts = new ConcurrentHashMap<>();
-private final Map<String, Long> timestamps = new ConcurrentHashMap<>();
-@Value("${rate.limit}")
-private int limit;
+    private final Map<String, Integer> requestCounts = new ConcurrentHashMap<>();
+    private final Map<String, Long> timestamps = new ConcurrentHashMap<>();
 
-@Value("${rate.window}")
-private long timeWindow;
+    @Value("${rate.limit}")
+    private int limit;
+
+    @Value("${rate.window}")
+    private long timeWindow;
+
+    @Autowired
+    private StatisticsService statisticsService;
 
     public boolean isAllowed(String apiKey) {
+
+        statisticsService.incrementTotal();
 
         long currentTime = System.currentTimeMillis();
 
         timestamps.putIfAbsent(apiKey, currentTime);
 
-       if (currentTime - timestamps.get(apiKey) > timeWindow){
+        if (currentTime - timestamps.get(apiKey) > timeWindow) {
             requestCounts.put(apiKey, 0);
             timestamps.put(apiKey, currentTime);
         }
 
         requestCounts.put(apiKey, requestCounts.getOrDefault(apiKey, 0) + 1);
 
-        return requestCounts.get(apiKey) <= limit;
+        boolean allowed = requestCounts.get(apiKey) <= limit;
+
+        if (allowed) {
+            statisticsService.incrementAllowed();
+        } else {
+            statisticsService.incrementBlocked();
+        }
+
+        return allowed;
     }
 }
